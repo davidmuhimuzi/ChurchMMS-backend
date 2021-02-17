de9aeaa9e25011edc7e74081bb43da7d4b01973d
davisb14 <35746095+davisb14@users.noreply.github.com>
2021-02-17 17:07:51 -0600

    Create db.config.js

---
+++ config/db.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  HOST: 'localhost',
+  USER: 'root',
+  PASSWORD: '',
+  DB: 'congregations',
+  dialect: 'mysql',
+  pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+  },
+};
+//you can also try this with all of your localhost information
