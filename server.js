c2e138a5ccdc8a78221b8aa1bc5b9fb5d6216c7d
srathbun13 <shauna.rathbun@eagles.oc.edu>
2021-04-21 14:20:32 -0500

    lifeeventperson added to server.js

--- server.js
+++ server.js
@@ -55,6 +55,7 @@ require('./routes/auth.routes')(app);
 require('./routes/user.routes')(app);
 require("./routes/congregation.routes")(app);
 require("./routes/lifeevent.routes")(app);
+require("./routes/lifeeventperson.routes")(app);
 
 
 // set port, listen for requests
